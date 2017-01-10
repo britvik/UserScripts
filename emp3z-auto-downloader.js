1f8f2fa5ff5c5ac457cc557a8cc60d5368016675
britvik <andrej.britvik@gmail.com>
2017-01-10 19:37:55 +0100

    updated url to match modified domain of site

--- emp3z-auto-downloader.js
+++ emp3z-auto-downloader.js
@@ -2,11 +2,12 @@
 // @name         Auto mp3 downloader
 // @description  Automatic download of multiple mp3s from https://www.emp3z.com. Just insert list of songs into textarea, push the button and wait till it finishes.
 // @author       Bladito
-// @version      0.6.1
+// @version      0.6.2
 // @homepageURL  https://greasyfork.org/en/users/55159-bladito
-// @match        http://www.emp3x.ws/*
-// @match        http://www.emp3z.ws/*
-// @match        https://www.emp3z.com/*
+// @match        *://www.emp3x.ws
+// @match        *://www.emp3z.ws
+// @match        *://www.emp3z.co
+// @match        *://www.emp3z.com
 // @namespace    Bladito/auto-mp3-downloader
 // @grant        none
 // ==/UserScript==
