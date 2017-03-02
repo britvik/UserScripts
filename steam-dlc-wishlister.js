2c08a9060da5c6d8e59103093d3f2a719f6bbbc7
britvik <andrej.britvik@gmail.com>
2017-03-02 20:02:54 +0100

    added button to remove all dlcs from wishlist

--- steam-dlc-wishlister.js
+++ steam-dlc-wishlister.js
@@ -2,7 +2,7 @@
 // @name         Steam DLC wishlister
 // @description  Add all DLCs on wishlist by one click. Also you can remove game from wishlist directly from game store page.
 // @author       Bladito
-// @version      0.3.0
+// @version      0.4.0
 // @homepageURL  https://greasyfork.org/en/users/55159-bladito
 // @match        http://store.steampowered.com/app/*
 // @namespace    Bladito/steam-dlc-wishlister
@@ -13,13 +13,26 @@
 (function($) {
     'use strict';
 
-    addButton_addAllDlcsToWishlist();
+    addDlcButton();
+
     if ($('#add_to_wishlist_area').length === 0) { // game is wishlisted
         addButton_removeFromWishlist();
     }
     $('#add_to_wishlist_area').click(addButton_removeFromWishlist);
 
     //-------------------------------------------------------------------------------------------------
+
+    // Used timeout because sometimes userdata is not cached and steam loads it after I add buttons
+    // (http://store.steampowered.com/dynamicstore/userdata/?id=12345&v=12345)
+    function addDlcButton() {
+        setTimeout(function() {
+            if ($('.game_area_dlc_row').length === $('.game_area_dlc_row.ds_wishlist').length) {
+                addButton_removeAllDlcsFromWishlist();
+            } else {
+                addButton_addAllDlcsToWishlist();
+            }
+        }, 1000);
+    }
 
     function addButton_removeFromWishlist() {
         var unwishlisterButton = $('<a class="btnv6_blue_hoverfade btn_medium queue_btn_inactive"><span>Remove from wishlist</span></a>');
@@ -28,9 +41,15 @@
     }
 
     function addButton_addAllDlcsToWishlist() {
-        var wishlisterButton = $('<a class="btnv6_blue_blue_innerfade btn_medium"><span>Add all DLC to Wishlist</span></a>');
+        var wishlisterButton = $('<a class="bladito-steam-wishlist btnv6_blue_blue_innerfade btn_medium"><span>Add all DLC to Wishlist</span></a>');
         wishlisterButton.click(addAllDlcsToWishlist);
         $('#dlc_purchase_action').prepend(wishlisterButton);
+    }
+
+    function addButton_removeAllDlcsFromWishlist() {
+        var unwishlisterButton = $('<a class="bladito-steam-unwishlist btnv6_blue_blue_innerfade btn_medium"><span>Remove all DLC from Wishlist</span></a>');
+        unwishlisterButton.click(removeAllDlcsFromWishlist);
+        $('#dlc_purchase_action').prepend(unwishlisterButton);
     }
 
     function addAllDlcsToWishlist() {
@@ -39,6 +58,24 @@
             unsafeWindow.AddToWishlist($this.data('ds-appid'));
             $this.addClass('ds_wishlist').addClass('ds_flagged');
             $this.append('<div class="ds_flag ds_wishlist_flag">ON WISHLIST&nbsp;&nbsp;</div>');
+        }).promise().done(function(){
+            $('.bladito-steam-wishlist').remove();
+            addButton_removeAllDlcsFromWishlist();
+        });
+    }
+
+    function removeAllDlcsFromWishlist() {
+        $('.game_area_dlc_row.ds_wishlist').each(function() {
+            var $this = $(this);
+            unsafeWindow.GDynamicStore.ModifyWishlist($this.data('ds-appid'), true, function() {
+                $this.removeClass('ds_wishlist').removeClass('ds_flagged');
+                $('.ds_flag.ds_wishlist_flag', $this).remove();
+            }, function(){
+                console.log('Error occured while trying to unwishlist DLC');
+            });
+        }).promise().done(function(){
+            $('.bladito-steam-unwishlist').remove();
+            addButton_addAllDlcsToWishlist();
         });
     }
 
