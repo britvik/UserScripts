354e9c1b943f0fef4a764366fb5c31a416ad0f41
britvik <andrej.britvik@gmail.com>
2017-08-29 09:53:58 +0200

    Typing invalid giveaway id will make the input invalid and GA page won't open

--- sg-ga-input.js
+++ sg-ga-input.js
@@ -2,7 +2,7 @@
 // @name         SteamGifts GA input
 // @description  Adds input on top menu where you can enter 5characters of GA id and it will open the GA page for you.
 // @author       Bladito
-// @version      0.1.0
+// @version      1.0.0
 // @homepageURL  https://greasyfork.org/en/users/55159-bladito
 // @match        https://www.steamgifts.com/*
 // @namespace    Bladito/sg-ga-input
@@ -17,7 +17,15 @@
     $input.on('input', function(event) {
         var value = event.target.value;
         if (value.length === 5) {
-            window.open('https://www.steamgifts.com/giveaway/'+value+'/', '_blank');
+            var url = 'https://www.steamgifts.com/giveaway/' + value + '/';
+            $.get(url, function(data) {
+                if ($(data).find('.sidebar__navigation__item__name').text().indexOf('Entries') > -1) { // it is a giveaway page
+                    window.open(url, '_blank');
+                    $('#bsg-ga-input').removeClass('bsg-invalid');
+                } else {
+                    $('#bsg-ga-input').addClass('bsg-invalid');
+                }
+            });
         }
     });
     $('.nav__left-container').append($input);
@@ -36,6 +44,13 @@
         GM_addStyle('#bsg-ga-input:focus { ' +
                     'color: black;' +
                     'background-color: white;' +
+                    '}');
+        GM_addStyle('#bsg-ga-input.bsg-invalid { ' +
+                    'border: 1px solid red;' +
+                    '}');
+        GM_addStyle('#bsg-ga-input.bsg-invalid:focus { ' +
+                    'background-color: #ffd2d2;' +
+                    'border: 1px solid red;' +
                     '}');
     }
 
